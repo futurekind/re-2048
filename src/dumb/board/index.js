e130cd369ef1a0143c4ec0214d0a4470d255957e
Fabian Trinler <fabian.trinler@blateral.com>
2016-08-09 16:53:34 +0200

    Adding basic board component

---
+++ src/dumb/board/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './board.scss';
+
+const Board = (props) => (
+    <div className="board"></div>
+)
+
+Board.defaultProps = {};
+
+Board.propTypes = {};
+
+export default Board
--- src/smart/app/index.js
+++ src/smart/app/index.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react';
 
+import Board from '../../dumb/board';
+
 import './app.scss';
 
 class App extends Component {
@@ -7,6 +9,7 @@ class App extends Component {
     render() {
         return (
             <div className="app">
+                <Board />
             </div>
         )
     }
