2233e15698011fa74187ed34d3532695ddaa862c
Fabian Trinler <fabian.trinler@blateral.com>
2016-08-10 10:21:03 +0200

    Adding tile reducer

--- src/redux/reducer.root.js
+++ src/redux/reducer.root.js
@@ -1,3 +1,7 @@
 import {combineReducers} from 'redux';
 
-export default combineReducers({})
+import tiles from './reducer.tile';
+
+export default combineReducers({
+    tiles
+})
---
+++ src/redux/reducer.tile.js
@@ -0,0 +1,10 @@
+export default (state = [], action) => {
+    switch (action.type) {
+        case 'ADD_TILE':
+            return state;
+            break;
+
+        default:
+            return state
+    }
+}
