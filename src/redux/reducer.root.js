2d696e93fbe83106d6174627fea478233bb5b711
Fabian Trinler <fabian.trinler@blateral.com>
2016-08-10 11:06:30 +0200

    Adding tile selectors

--- src/redux/reducer.root.js
+++ src/redux/reducer.root.js
@@ -1,7 +1,10 @@
 import {combineReducers} from 'redux';
 
-import tiles from './reducer.tile';
+import tiles, * as fromTiles from './reducer.tile';
 
 export default combineReducers({
     tiles
 })
+
+export const getEmptyFields = (state) => fromTiles.getEmptyFields(state);
+export const getAllFields = () => fromTiles.getAllFields();
--- src/redux/reducer.tile.js
+++ src/redux/reducer.tile.js
@@ -1,10 +1,35 @@
-export default (state = [], action) => {
+const fieldsMap = [
+    [0, 0], [0, 1], [0, 2], [0, 3],
+    [1, 0], [1, 1], [1, 2], [1, 3],
+    [2, 0], [2, 1], [2, 2], [2, 3],
+    [3, 0], [3, 1], [3, 2], [3, 3]
+]
+
+export default (state = [
+    {uuid: '123', value: 2, fieldMapIndex: 0},
+    {uuid: '456', value: 4, fieldMapIndex: 15},
+], action) => {
     switch (action.type) {
         case 'ADD_TILE':
             return state;
             break;
 
         default:
-            return state
+            return state;
     }
 }
+
+export const getEmptyFields = (state) => {
+    const fieldMapIndexes = state.map(tile => tile.fieldMapIndex)
+
+    return fieldsMap.reduce((emptyFieldMap, field, index) => {
+        if(fieldMapIndexes.indexOf(index) === -1)
+            return [
+                ...emptyFieldMap,
+                field
+            ]
+        return emptyFieldMap
+    }, []);
+}
+
+export const getAllFields = () => fieldsMap
